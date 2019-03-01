e249e5986bc85178435e7d80a7e0937acdce64c8
Pablo Zapata Rodriguez <pablo.zapata@estudiante.uam.es>
2019-03-01 19:56:37 +0100

    01/03

--- P1/Perceptron/perceptron.py
+++ P1/Perceptron/perceptron.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from redNeuronal import RedNeuronal
 
 
+class Perceptron(RedNeuronal):
 
-class Perceptron():
-
-    def __init__(self, alpha, umbral, tamentrada, tamsalida):
-        self.alpha = alpha
+    def __init__(self, alpha, umbral, tamentrada, tamsalida, max_epocas):
+        #super().__init__(tamentrada, tamsalida, [],alpha,umbral, max_epocas)
+        self.max_epocas=max_epocas
+        self.alpha= alpha
         self.umbral = umbral
         self.pesos=np.zeros((tamsalida,tamentrada+1)) #inicializamos pesos y sesgo a 0 (una fila por neurona de salida, tantas columnas como neuronas de entrada + el sesgo)
 
@@ -17,9 +19,9 @@ class Perceptron():
         contador=1
         pesos_nueva_epoca=self.pesos
         pesos_actual=np.ones_like(self.pesos)
-        while contador <=100 and not np.array_equal(pesos_nueva_epoca,pesos_actual) :
+        while contador <=self.max_epocas and not np.array_equal(pesos_nueva_epoca,pesos_actual) :
             pesos_nueva_epoca=self.pesos.copy()
-            print ("EPOCA " + str(contador) + "\nPesos al inicio: " + str(pesos_nueva_epoca)) 
+            print ("EPOCA " + str(contador) + "\nPesos al inicio: " + str(pesos_nueva_epoca))
             #para cada dato de entrenamiento
             for dato in range(datostrain.shape[0]):
                 pesos_actual= self.pesos
@@ -44,7 +46,7 @@ class Perceptron():
 
     def predict(self, datostest, tamsalida):
         predicciones=[]
-        
+
         for dato in range(datostest.shape[0]):
             entrada = np.append(datostest[dato],1) #establecemos activaciones de las neuronas de entrada (el ultimo es para el sesgo)
             prediccion=[]
@@ -68,6 +70,14 @@ class Perceptron():
         return err/datostest.shape[0]
 
 
+if __name__ == "__main__":
+    from particionado import *
 
-                    
-        
+    modo = Modo1("./data/problema_real1.txt", 0.8)
+
+    p = Perceptron(0.3,0,modo.X_train.shape[1], modo.y_train.shape[1],100)
+    p.train(modo.X_train, modo.y_train)
+
+    predicciones = p.predict(modo.X_test, modo.y_test.shape[1])
+
+    print("Porcentaje de error al clasificar los datos: " + str(p.test(predicciones,modo.y_test)))
