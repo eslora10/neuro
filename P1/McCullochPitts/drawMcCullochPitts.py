a276d798ab160b610da27cdd7ae4c93b9077ad4c
esther <eslora96@gmail.com>
2019-03-03 19:16:07 +0100

    Terminado (creo) McCullochPitts

---
+++ P1/McCullochPitts/drawMcCullochPitts.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.DiGraph()
+G.add_edge("x1", "z11", weight=2)
+G.add_edge("x1", "z22", weight=1)
+G.add_edge("x1", "z33", weight=1)
+G.add_edge("x2", "z12", weight=2)
+G.add_edge("x2", "z23", weight=1)
+G.add_edge("x2", "z31", weight=1)
+G.add_edge("x3", "z13", weight=2)
+G.add_edge("x3", "z21", weight=1)
+G.add_edge("x3", "z32", weight=1)
+G.add_edge("z11", "z21", weight=1)
+G.add_edge("z11", "z31", weight=1)
+G.add_edge("z12", "z22", weight=1)
+G.add_edge("z12", "z32", weight=1)
+G.add_edge("z13", "z23", weight=1)
+G.add_edge("z13", "z33", weight=1)
+G.add_edge("z21", "y1", weight=2)
+G.add_edge("z22", "y1", weight=2)
+G.add_edge("z23", "y1", weight=2)
+G.add_edge("z31", "y2", weight=2)
+G.add_edge("z32", "y2", weight=2)
+G.add_edge("z33", "y2", weight=2)
+
+pos = {
+    "x1": (0,2), "x2": (0,1), "x3": (0,0),
+    "z11": (1,2), "z12": (1,1), "z13": (1,0),
+    "z21": (2,2), "z22": (2,1), "z23": (2,0),
+    "z31": (2,1.5), "z32": (2, 0.5), "z33": (2, -0.5),
+    "y1": (3, 1.5), "y2": (3, 0.5)
+}
+
+plt.title(r'$\theta=2$')
+nx.draw(G, arrows = True, with_labels = True, pos = pos, node_size= 500, node_color = "w", alpha = 0.8)
+labels = nx.get_edge_attributes(G,'weight')
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+ax= plt.gca()
+ax.collections[0].set_edgecolor("#000000")
+plt.show()
+plt.savefig("RedMcCullochPitts.png")
---
+++ P1/McCullochPitts/mcCullochPitts.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+umbral = 2
+red = { "x1": {"valor": 0, "incidencia": {}},
+        "x2": {"valor": 0, "incidencia": {}},
+        "x3": {"valor": 0, "incidencia": {}},
+        "z11": {"valor": 0, "incidencia": {"x1": 2}},
+        "z12": {"valor": 0, "incidencia": {"x2": 2}},
+        "z13": {"valor": 0, "incidencia": {"x3": 2}},
+        "z21": {"valor": 0, "incidencia": {"x3": 1, "z11": 1}},
+        "z22": {"valor": 0, "incidencia": {"x1": 1, "z12": 1}},
+        "z23": {"valor": 0, "incidencia": {"x2": 1, "z13": 1}},
+        "z31": {"valor": 0, "incidencia": {"x2": 1, "z11": 1}},
+        "z32": {"valor": 0, "incidencia": {"x3": 1, "z12": 1}},
+        "z33": {"valor": 0, "incidencia": {"x1": 1, "z13": 1}},
+        "y1": {"valor": 0, "incidencia": {"z21": 2, "z22": 2, "z23": 2}},
+        "y2": {"valor": 0, "incidencia": {"z31": 2, "z32": 2, "z33": 2}},
+}
+
+def respuesta(red, x1, x2, x3):
+    red["x1"]["valor"] = x1
+    red["x2"]["valor"] = x2
+    red["x3"]["valor"] = x3
+
+    siguientes = ["y1", "y2"]
+    while siguientes:
+        nodo = siguientes[0]
+        incidencia = red[nodo]["incidencia"]
+        if nodo != "x1" and nodo != "x2" and nodo != "x3":
+            red[nodo]["valor"] = 0
+            for conexion, peso in incidencia.items():
+                red[nodo]["valor"] += peso*red[conexion]["valor"]
+                if conexion not in siguientes:
+                    siguientes.append(conexion)
+            red[nodo]["valor"] = int(red[nodo]["valor"] >= umbral)
+        siguientes.remove(nodo)
+
+    print(red["y1"]["valor"], red["y2"]["valor"])
+
+with open("McCulloch_Pitts.txt") as entrada:
+    for line in entrada:
+        parsed = line.strip("\n").split()
+        x1 = int(parsed[0])
+        x2 = int(parsed[1])
+        x3 = int(parsed[2])
+        respuesta(red, x1, x2, x3)
+
+    respuesta(red, 0, 0, 0)
