3733e2d3199d3cbc31893c26b6dc264fe7de50c4
Esther <esther.lopezramos@estudiante.uam.es>
2019-05-07 00:13:44 +0200

    añadidas graficas que comparan los datos de train con las predicciones

--- P3/src/SerieTemp.py
+++ P3/src/SerieTemp.py
@@ -8,7 +8,7 @@ from per_multicapa import PerceptronMulticapa
 parser = argparse.ArgumentParser(description='Entrena y evalua la prediccion de series temporales')
 parser.add_argument('fich_entrada', help='fichero con la serie en el formato correcto')
 parser.add_argument('capas', type=int, metavar='ncapas', nargs='+', help='neuronas de las capas intermedias')
-parser.add_argument('--tam_train', type=float, default=0.7, help="porcentaje de datos en train")
+parser.add_argument('--tam_train', type=int, default=100, help="porcentaje de datos en train")
 parser.add_argument('--alpha', type=float, default=0.1, help="constante de aprendizaje")
 parser.add_argument('--nepocas', type=int, default=100, help="numero de epocas de entrenamiento")
 parser.add_argument('--tol', type=float, default=0, help="tolerancia para el entrenamiento")
@@ -32,14 +32,36 @@ red.train(datos.X_train, datos.y_train)
 
 # Test
 prediction = red.predict(datos.X_test)
-for i in range(5):
-    print(datos.y_test[i], prediction[i])
 
 # Prediccion recursivas
 x = datos.X_test[-1]
 y = datos.y_test[-1]
-print(args.npred)
+rec = []
 for _ in range(args.npred):
     x = np.concatenate((x, y))[-datos.num_atributos:]
     y = red.predict(np.array([x]))[0]
-    print(x, y)
+    rec+=list(x)
+    #print(x, y)
+rec+=list(y)
+
+import matplotlib.pyplot as plt
+teorico =[]
+for dato in datos.X_train:
+    teorico.append(dato[0])
+for dato in datos.X_test:
+    teorico.append(dato[0])
+teorico += list(datos.y_test[-1])
+predicho = []
+for dato in prediction:
+    predicho.append(dato[0])
+predicho+=list(prediction[-1])
+t = range(len(teorico))
+print(len(teorico))
+plt.plot(t, teorico, label = "valor teorico")
+plt.plot(t[-len(predicho):], predicho, color='r', label = "valor predicho")
+if args.npred != 0:
+    plt.plot(range(len(t), len(t)+len(rec)), rec, label = "prediccion recursiva")
+plt.xlabel("t")
+plt.ylabel("valor de la serie")
+plt.legend()
+plt.show()
